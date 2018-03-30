4f8e88d3eeaa1e5825e0181ca8ddf9f847714f53
Dabudi <30605480+Dabudi@users.noreply.github.com>
2018-03-30 23:23:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task1
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+            double N = double.Parse(input[0]);
+            double M = double.Parse(input[1]);
+            Console.WriteLine((N > 3*M) ? Math.Ceiling((N-3*M)/2) : 0);
+        }
+    }
+}

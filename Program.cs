7e4cb7687dec0bcee2fe07f4269353891f88adf0
Dabudi <30605480+Dabudi@users.noreply.github.com>
2018-03-30 23:25:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+            int N = int.Parse(input[0]);
+            int K = int.Parse(input[1]);
+            ulong prod = 1;
+
+            prod = (ulong)Math.Pow(Factorial(N)/(Factorial(N-K) * Factorial(K)), 2);
+            Console.WriteLine(prod*Factorial(K));
+        }
+
+        static ulong Factorial(int N)
+        {
+            if (N == 0) ;
+            ulong prod = 1;
+            for(ushort i = 2; i <= N; ++i)
+            {
+                prod *= i;
+            }
+            return prod;
+        }
+    }
+}
